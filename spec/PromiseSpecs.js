af4d730f6062dcaa7423783c1254011bcac8f881
Raoul Millais <raoul@raoulmillais.com>
2013-04-05 15:37:24 +0100

    Simple promise for external commands

---
+++ spec/PromiseSpecs.js
@@ -0,0 +1,103 @@
+var chai = require('chai');
+var expect = chai.expect;
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+var Promise = require('../lib/promise.js');
+
+chai.use(sinonChai);
+
+describe('Promise', function () {
+
+	it('should default success and failure', function () {
+		var promise = new Promise();
+
+		expect(promise.successCallback).to.exist;
+		expect(promise.failureCallback).to.exist;
+	});
+
+	it('should add success and failure callbacks', function () {
+		var promise = new Promise();
+		var succeed = function succeed(data) {};
+		var fail = function fail(err, data) {};
+
+		promise.addCallbacks(succeed, fail);
+
+		expect(promise.successCallback).to.equal(succeed);
+		expect(promise.failureCallback).to.equal(fail);
+	});
+
+	it('should throw when no success and failure callbacks are given', function () {
+		var promise = new Promise();
+		
+		expect(promise.addCallbacks).to.throw(Error);
+	});
+
+	it('should call success callback when resolving', function () {
+		var promise = new Promise();
+		var succeed = sinon.spy();
+		var fail = sinon.spy();
+
+		promise.addCallbacks(succeed, fail);
+		promise.resolve('YAY!');
+
+		expect(promise.successCallback).to.have.been.called;
+		expect(promise.failureCallback).not.to.have.been.called;
+	});
+
+	it('should emit complete event when resolving', function () {
+		var promise = new Promise();
+		var succeed = sinon.spy();
+		var fail = sinon.spy();
+		var complete = sinon.spy();
+
+		promise.addCallbacks(succeed, fail);
+		promise.on('complete', complete);
+		promise.resolve('YAY!');
+
+		expect(complete).to.have.been.called;
+	});
+
+	it('should emit complete event when rejecting', function () {
+		var promise = new Promise();
+		var succeed = sinon.spy();
+		var fail = sinon.spy();
+		var complete = sinon.spy();
+
+		promise.addCallbacks(succeed, fail);
+		promise.on('complete', complete);
+		promise.reject('YAY!');
+
+		expect(complete).to.have.been.called;
+	});
+
+	it('should call failure callback when rejecting', function () {
+		var promise = new Promise();
+		var succeed = sinon.spy();
+		var fail = sinon.spy();
+
+		promise.addCallbacks(succeed, fail);
+		promise.reject(new Error());
+
+		expect(promise.successCallback).not.to.have.been.called;
+		expect(promise.failureCallback).to.have.been.called;
+	});
+
+	it('should throw when no failure callback and rejecting', function () {
+		var promise = new Promise();
+		var succeed = sinon.spy();
+		var error = new Error();
+		var threw = false;
+
+		promise.addCallbacks(succeed);
+		try {
+			promise.reject(error);
+		} catch (err) {
+			threw = true;
+			expect(err).to.eql(error);
+		}
+		
+		expect(threw).to.equal(true);
+		expect(promise.successCallback).not.to.have.been.called;
+	});
+
+});
